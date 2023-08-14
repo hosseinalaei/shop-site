1333e4e74e670a35ec04765edd13481f6f374274
Mehrad <mehradhetfield@gmail.com>
2023-08-14 11:27:06 +0330

    feat: breadcrumb component created

---
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -0,0 +1,14 @@
+const Breadcrumb = () => {
+    return ( 
+        <nav class="breadcrumb-nav mb-10 pb-1">
+                <div class="container">
+                    <ul class="breadcrumb">
+                        <li><a href="demo1.html">خانه </a></li>
+                        <li>تماس با ما</li>
+                    </ul>
+                </div>
+            </nav>
+     );
+}
+ 
+export default Breadcrumb;
