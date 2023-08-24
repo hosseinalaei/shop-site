8926ab247f7ff3af90e016050c7bf2ce42b2a26e
hosseinalaei <89533880+hosseinalaei@users.noreply.github.com>
2023-08-24 19:21:12 +0330

    feat: category&product api added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
+import { Suspense } from "react";
 import Footer from "../Footer/Footer";
 import StickyFooter from "../Footer/StickyFooter";
 import Header from "../Header/Header";
+import Loading from "../Loading/Loading";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      {children}
+      <Suspense fallback={<Loading />}>{children}</Suspense>
       <Footer />
       <StickyFooter />
     </>
