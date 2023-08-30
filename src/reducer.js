5da046ec6e0baa1b06ab945397dd016331572a19
Mehrad <mehradhetfield@gmail.com>
2023-08-30 21:33:37 +0330

    feat: deduction logic implemented

--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,6 @@ const reducer = (state, action) => {
       return { ...state, cart: [] };
     }
     case "REMOVE": {
-      console.log(state);
       return {
         ...state,
         cart: state.cart.filter((item) => item.id !== action.payload),
@@ -51,7 +50,24 @@ const reducer = (state, action) => {
         return { ...state, cart: tempCart };
       }
     }
+    case "DEDUCTION" :{
+      console.log(action.payload.quantity);
+      if(action.payload.quantity === 1){
+        console.log(action.payload);
+        return {...state, cart: state.cart.filter(item => item.id !== action.payload.id)}
+      } 
+      else{
+        const tempCart = state.cart.map((item) => {
+          if (item.id === action.payload.id && action.payload.quantity > 0) {
+            return { ...item, quantity: item.quantity-1 };
+          }
+          return item;
+        });
+        return { ...state, cart: tempCart };
+      }
+    }
   }
+  
 };
 
 export default reducer;
