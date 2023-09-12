bf50b1f9e90bfdf20749046331a3e3a31018b4f2
Mehrad <mehradhetfield@gmail.com>
2023-09-12 13:19:29 +0330

    refactor: mobile menu added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,21 @@ import Footer from "../Footer/Footer";
 import StickyFooter from "../Footer/StickyFooter";
 import Header from "../Header/Header";
 import Loading from "../Loading/Loading";
+import MobileMenu from "../Menu/MobileMenu";
+import { useCartContext } from "@/contexts/contex";
+import Backdrop from "../FiltersDrawer/Backdrop";
 
 const Layout = ({ children }) => {
+  const {mobileMenu, setMobileMenu} = useCartContext();
+  console.log(mobileMenu);
   return (
     <>
+      {mobileMenu && <MobileMenu mobileMenu={mobileMenu}/>}
+      {mobileMenu && (
+            <>
+              <Backdrop close={() => setMobileMenu(false)} />
+            </>
+          )}
       <Header />
       <Suspense fallback={<Loading />}>{children}</Suspense>
       <Footer />
